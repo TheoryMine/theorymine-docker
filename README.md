0b77fa70bd476fdaddef4b37c6d8ba43a1521e24
Lucas Dixon <lucas.dixon@gmail.com>
2015-10-11 22:04:05 -0400

    filename cleanup; made IsaPlanner docker file

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 theorymine-docker
 =================
 
-This repo contains the tools and Docker image to setup TheoryMine. You first need to have [docker](https://www.docker.com/) installed (and/or with boot2docker). 
+This repo contains the tools and Docker image to setup TheoryMine. You first need to have [docker](https://www.docker.com/) installed (and/or with boot2docker).
 Note, if you install on linux without boot2docker, then all your docker commands need to be run as superuser (i.e. prefixed with the sudu command). If you have docker and boot2docker, then this command starts up boot2docker which, after entering the command lines it tells you to set some environment variables, will allow you to run the docker command.
 
 ```
@@ -9,16 +9,21 @@ Note, if you install on linux without boot2docker, then all your docker commands
 boot2docker up
 
 # Get a checkout of theorymine-docker.
-git clone git@github.com:TheoryMine/theorymine-docker.git 
+git clone git@github.com:TheoryMine/theorymine-docker.git
 cd theorymine-docker
 
-# Build the docker file; this will take a while (has to download ubuntu, 
+# Build the docker file; this will take a while (has to download ubuntu,
 # and then install everything, and then build all the
 # Isabelle/IsaPlanner/IsaCosy images).
 docker build -t theorymine/theorymine-image .
 ```
 
-To enter the docker environment and get a bash shell there, you can then run: 
+To enter the docker environment and get a bash shell there, you can then run:
+```
+sudo docker run -t -i theorymine:theorymine-image /bin/bash
+```
+
+To enter a docker environment with IsaPlanner setup and get a bash shell there, you can then run:
 ```
 sudo docker run -t -i theorymine:theorymine-image /bin/bash
 ```
--- setup.sh
+++ setup.sh
@@ -1,9 +1,79 @@
 #/!bin/bash
 set -e
 
-# Local setup for docker.
-mkdir -p shared_directories
-git clone git@github.com:TheoryMine/math-robot.git \
-  shared_directories/math-robot
-git clone git@github.com:TheoryMine/theorymine-website.git \
-  shared_directories/theorymine-website
+# Get the directory where this script is and set ROOT_DIR to that path. This
+# allows script to be run from different directories but always act on the
+# directory it is within.
+ROOT_DIR="$(cd "$(dirname $0)"; pwd)";
+TMP_DIR="$ROOT_DIR/tmp"
+SHARED_DIR="$ROOT_DIR/shared_directory"
+
+
+#--------------------------------------
+# Num  Colour    #define         R G B
+#--------------------------------------
+# 0    black     COLOR_BLACK     0,0,0
+# 1    red       COLOR_RED       1,0,0
+# 2    green     COLOR_GREEN     0,1,0
+# 3    yellow    COLOR_YELLOW    1,1,0
+# 4    blue      COLOR_BLUE      0,0,1
+# 5    magenta   COLOR_MAGENTA   1,0,1
+# 6    cyan      COLOR_CYAN      0,1,1
+# 7    white     COLOR_WHITE     1,1,1
+
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+BLUE=`tput setaf 4`
+RESET=`tput sgr0`
+
+function echo_green {
+  echo "${GREEN}$1${RESET}"
+}
+
+function echo_red {
+  echo "${RED}$1${RESET}"
+}
+
+function echo_blue {
+  echo "${BLUE}$1${RESET}"
+}
+
+
+# A simple bash script to run commands to setup and install all dev dependencies
+# (including non-npm ones)
+function runAndAssertCmd ()
+{
+    echo_green "$1"
+    echo
+    # We use set -e to make sure this will fail if the command returns an error
+    # code.
+    set -e
+    cd $ROOT_DIR && eval $1
+}
+
+# Just run the command, ignore errors (e.g. cp fails if a file already exists
+# with "set -e")
+function runCmd ()
+{
+    echo_green "$1"
+    echo
+    cd $ROOT_DIR && eval $1
+}
+
+# Setup local directories that will be shared with the docker containers.
+runAndAssertCmd "mkdir -p shared_directories"
+
+if [! -a "$SHARED_DIR/math-robot" ] then
+ runAndAssertCmd "git clone git@github.com:TheoryMine/math-robot.git \
+   shared_directories/math-robot"
+fi
+if [! -a "$SHARED_DIR/theorymine-website" ] then
+ runAndAssertCmd "git clone git@github.com:TheoryMine/theorymine-website.git \
+   shared_directories/theorymine-website"
+fi
+
+runAndAssertCmd "docker build -t theorymine/isaplanner:2009.2 \
+  ./isaplanner_docker/"
+
+# The tmp directory is for
+runAndAssertCmd "mkdir -p tmp"
